2b8e39a1cc0ba45d2b018a058b861c61ce2040b6
Gurthog <226201902+Gurthog@users.noreply.github.com>
2025-10-06 19:58:53 -0700

    CH3 L1 submit

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from stats import word_count, char_count
+from stats import (
+    word_count,
+    char_count,
+    sort_dict_by_value,
+)
 
 
 def main():
-    FRANK = r"books/frankenstein.txt"
-    book_text = get_book_text(FRANK)
+    book_path = r"books/frankenstein.txt"
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    book_text = get_book_text(book_path)
+
+    print("----------- Word Count ----------")
     wc = word_count(book_text)
     print(f"Found {wc} total words")
 
-    chars = char_count(book_text)
-    print(chars)
+    print("--------- Character Count -------")
+    chars_dict = char_count(book_text)
+    sorted_dicts = sort_dict_by_value(chars_dict)
+    for d in sorted_dicts:
+        if d["char"].isalpha():
+            print(f"{d["char"]}: {d["num"]}")
+
+    print("============= END ===============")
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -9,3 +9,14 @@ def char_count(text):
             counts[c] = 0
         counts[c] += 1
     return counts
+
+
+def sort_dict_by_value(d: dict) -> list[dict]:
+    def sort_on(item):
+        return item["num"]
+
+    items = []
+    for key in d:
+        items.append({"char": key, "num": d[key]})
+    items.sort(key=sort_on, reverse=True)
+    return items
